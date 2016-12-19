bda8077649f9789879fea92451d1ef9b909a093c
Osanchmo <sanmoroscar@gmail.com>
2016-12-19 20:00:35 +0100

    jsonlib

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -13,6 +13,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Magic The Gathering");
         primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.getScene().getStylesheets().add("style.css");
         primaryStage.show();
     }
 
